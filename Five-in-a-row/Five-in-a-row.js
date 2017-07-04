ba4ca59e56aa39a58c2edd6ee6685e7ab8942523
Guohjia <guohjia@163.com>
2017-07-04 23:05:14 +0800

    下棋位置精确定位

--- Five-in-a-row/Five-in-a-row.js
+++ Five-in-a-row/Five-in-a-row.js
@@ -1,17 +1,41 @@
 
 
-var border = document.querySelector('.border')
+var board = document.querySelector('.board')
 var nextChess='white'  //初始化接下来是哪方下棋
 var symbol=document.querySelector('.turn')
-border.addEventListener('click',function(e){
-    var coordinateX=e.target.offsetLeft;
-    var coordinateY=e.target.offsetTop;
-    console.log(coordinateX)
+board.addEventListener('click',function(e){
+    var clickX=e.clientX+window.scrollX  //踩坑一：因为小格子offsetLeft是相对于父元素board计算的，而clientX是相对于浏览器窗口计算的，因此出现滚动条并滚动会出现bug，所以要加上window.scrollX;scrollY同理;
+    var clickY=e.clientY+window.scrollY  
+    var coordinateX=e.target.offsetLeft
+    var coordinateY=e.target.offsetTop
+    // console.log(clickX)
+    console.log(clickY)
+    // console.log(coordinateX)
     console.log(coordinateY)
     var newChess=document.createElement('div')
         newChess.className='chess'
-        newChess.style.left=coordinateX-20+'px'  //减去格子一半的宽度，保证棋子在格子左上角顶点
-        newChess.style.top=coordinateY-21+'px'   //减去格子一半的高度，保证棋子在格子左上角顶点
+
+    //纠正用户点击偏差，将每一个方格分成4份进行判断精准定位，                           
+    if(clickX-coordinateX<=20&&clickY-coordinateY<=21){  //出现左上角顶点判断;等号标识交界处归于左上角定点；
+         console.log('去左上角')
+        newChess.style.left=coordinateX-20+'px'  //减去格子一半的宽度，保证棋子中心在格子左上角顶点
+        newChess.style.top=coordinateY-21+'px'   //减去格子一半的高度，保证棋子中心在格子左上角顶点
+    }
+    if(clickX-coordinateX>20&&clickY-coordinateY<=21){  //出现右上角顶点判断;等号表示上下交界处的右边归属右上定点;
+        console.log('去右上角')
+        newChess.style.left=coordinateX+20+'px'  
+        newChess.style.top=coordinateY-21+'px'   
+    }
+    if(clickX-coordinateX<=20&&clickY-coordinateY>21){  //出现左下角顶点判断;等号表示上下交界处的右边归属左下定点;
+        console.log('去左下角')
+        newChess.style.left=coordinateX-20+'px'  
+        newChess.style.top=coordinateY+21+'px'   
+    }
+    if(clickX-coordinateX>20&&clickY-coordinateY>21){  //出现右下角顶点判断;
+        console.log('去右上角')
+        newChess.style.left=coordinateX+20+'px'  
+        newChess.style.top=coordinateY+21+'px'   
+    }    
         //判断下棋顺序
         if(nextChess=='white') {
             newChess.style.background="white"
