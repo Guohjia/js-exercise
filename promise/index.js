d6e6b396e6ef4f5977b21fabecbaf1c43e0f0626
Guohjia <guohjia@163.com>
2018-03-13 19:44:01 +0800

    分离职责,then添加到promise对象中

--- promise/index.js
+++ promise/index.js
@@ -12,26 +12,16 @@ var defer = function () {
                 throw new Error("只能被resolve一次啊.")
             }
         },
-        then: function (_callback) {
-            if (pending) {
-                pending.push(_callback)
-            } else {
-                _callback();
+        promise: {
+            then: function (_callback) {
+                if (pending) {
+                    pending.push(_callback)
+                } else {
+                    _callback();
+                }
+                //
             }
-            //
-        }
+        },
     }
 }
 
-let oneOneSecondLater = () => {
-    let result = defer();
-    setTimeout(() => {
-        result.resolve(1); //一秒后传入值,并且执行callback
-        result.resolve(1);
-    }, 1000);
-    return result;
-};
-
-oneOneSecondLater().then((i) => {
-    console.log('这就是,我就是异步then,我只为自己代言哈哈哈哈'+i)
-})  //第二个then无法实现链式调用
