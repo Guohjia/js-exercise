35e8069c989f34e2a0c52c9001378a2dcfae728d
Guohjia <guohjia@163.com>
2018-03-13 20:31:48 +0800

    重写promise

--- promise/index.js
+++ promise/index.js
@@ -1,27 +1,23 @@
-var defer = function () {
-    let pending = [], value;
-    return {
-        resolve: function (_value) {
-            if(pending){
-                value = _value;
-                for (var i = 0; i < pending.length; i++) {
-                    pending[i](value)
-                }
-                pending = undefined;
-            }else{
-                throw new Error("只能被resolve一次啊.")
-            }
-        },
-        promise: {
-            then: function (_callback) {
-                if (pending) {
-                    pending.push(_callback)
-                } else {
-                    _callback();
-                }
-                //
-            }
-        },
+function Defer(executor) { //executor 用户传递的执行函数,参数分别为resolve与reject;
+    if (!(this instanceof Defer)) { //
+        throw 'Defer is a constructor and should be called width "new" keyword';
+    }
+    if (typeof executor !== 'function') {
+        throw 'Defer params must be a function';
+    }
+    try {
+        executor.call(this, this.resolve.bind(this), this.reject.bind(this));//传递resolve，reject方法
+    } catch (e) {
+        this.reject(e);  //函数中执行错误立即调用reject
     }
 }
 
+Defer.prototype = {
+    constructor: Defer,
+    resolve: function (value) {
+        this.value = value;//缓存代理的值
+    },
+    reject: function (reason) {
+        this.rejectReason = reason;//缓存失败原因
+    }
+}
